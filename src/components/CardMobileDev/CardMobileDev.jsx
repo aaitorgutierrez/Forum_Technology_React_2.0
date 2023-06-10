7f5ecf01027a2f41797f0c113cd311811dfe34f8
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-06-10 13:06:16 +0100

    Css Cartas

--- src/components/CardMobileDev/CardMobileDev.jsx
+++ src/components/CardMobileDev/CardMobileDev.jsx
@@ -6,18 +6,19 @@ export const CardMobileDev = ({ mobileDev }) => {
   const navigate = useNavigate();
   const pathById = `/mobileDev/apps`;
 
-  console.log('mobileDev: ', mobileDev)
+  console.log("mobileDev: ", mobileDev);
   return (
     <div className="mobileDev-Info">
       {/* <p>{mobileDev.brand}</p> */}
       <img src={mobileDev.image} alt={`mobileDev's ${mobileDev.brand} pic`} />
-      <p>OS: {mobileDev.OS}</p>
-      <p>versionOS: {mobileDev.versionOS}</p>
-      <p>language: {mobileDev.language}</p>
-      <p>{mobileDev.description}</p>
+      <p> {mobileDev.OS}</p>
+      <p> {mobileDev.versionOS}</p>
+      <p> {mobileDev.language}</p>
+      <h5>{mobileDev.description}</h5>
       <div className="button-Holder">
         <ToggleButton className="ToggleMobileDev" mobileDevId={mobileDev._id} />
         <button
+          className="button-MobileDev"
           onClick={() =>
             navigate(pathById, {
               state: { mobileDevId: mobileDev._id, color: "blue" },
