1cc56254dbf2a96a4c6617ff7ff0256d04f55509
Aitor Gutierrez <gutierrezizquierdoaitor@gmail.com>
2023-06-10 20:18:08 +0200

    Header update

--- src/components/UserWelcome/UserWelcome.jsx
+++ src/components/UserWelcome/UserWelcome.jsx
@@ -1,25 +1,26 @@
 import { TbPlant2 } from "react-icons/tb";
 import { useAuth } from "../../contexts/authContext";
 
-import './UserWelcome.css'
+import "./UserWelcome.css";
 
 const UserWelcome = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
-    return (
-        <div className="userWelcome-container">
-            <p className="userWelcome-welcome-text">
-                Hi {user.user}! Thanks for stopping by.
-            </p>
-            <div className="userWelcome-logo">
-                <TbPlant2 size={30} />
-                <h3 className="titleWeb">Digital Nexus</h3>
-            </div>
-            <p className="userWelcome-info-text">
-                Our site is full of great resources and information, so feel free to explore.
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="userWelcome-container">
+      <TbPlant2 className="LogoDashboard" size={150} />
+      <p className="userWelcome-welcome-text">
+        Hi {user.user}! Thanks for stopping by.
+      </p>
+      <div className="userWelcome-logo">
+        <h3 className="titleWeb">Digital Nexus</h3>
+      </div>
+      <p className="userWelcome-info-text">
+        Our site is full of great resources and information, so feel free to
+        explore.
+      </p>
+    </div>
+  );
+};
 
-export default UserWelcome
+export default UserWelcome;
