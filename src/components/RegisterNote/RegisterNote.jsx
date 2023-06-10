81c10a6bc1dbf7bd36f6f26113e8e1f5a0375c02
Igor <iluzarraga@gmail.com>
2023-06-10 16:09:57 +0200

    Added new component RegiterNote

---
+++ src/components/RegisterNote/RegisterNote.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { TbPlant2 } from "react-icons/tb";
+import './RegisterNote.css'
+
+const RegisterNote = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="registerNote-container">
+            <p className="registerNote-welcome-text">
+                Welcome to our Forum of technology!
+            </p>
+            <div className="registerNote-logo">
+                <TbPlant2 size={30} />
+                <h3 className="titleWeb">Digital Nexus</h3>
+            </div>
+            <p className="registerNote-please-text">
+                Please, register to access the page!
+            </p>
+            <button className="registerNote-login-btn" onClick={() => navigate("/login")}>
+                LOGIN
+            </button>
+        </div>
+    )
+}
+
+export default RegisterNote
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/authContext";
 import "./Home.css";
+import RegisterNote from "../components/RegisterNote/RegisterNote";
 
 export const Home = () => {
   const { user } = useAuth();
@@ -10,16 +11,8 @@ export const Home = () => {
     <div className="homeContainer">
       {user !== null ? (
         <h1 className="titleHome">Bienvenido {user.user}</h1>
-      ) : (
-        <>
-          <h1 className="titleHome">
-            Por favor registrese para poder acceder a la pagina
-          </h1>
-          <button className="btn" onClick={() => navigate("/login")}>
-            IR A LOGIN
-          </button>
-        </>
-      )}
+      ) : <RegisterNote />
+      }
     </div>
   );
 };
