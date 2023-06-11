752d84b01e00f2aacaed59abc440e2574d9f5fd4
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-06-11 12:45:23 +0100

    CSS register Note y Tamaño Nav

--- src/components/CardMobileDev/CardMobileDev.jsx
+++ src/components/CardMobileDev/CardMobileDev.jsx
@@ -11,9 +11,28 @@ export const CardMobileDev = ({ mobileDev }) => {
     <div className="mobileDev-Info">
       {/* <p>{mobileDev.brand}</p> */}
       <img src={mobileDev.image} alt={`mobileDev's ${mobileDev.brand} pic`} />
-      <p> {mobileDev.OS}</p>
-      <p> {mobileDev.versionOS}</p>
-      <p> {mobileDev.language}</p>
+      <div className="mobileDev-Info-tabla">
+        <div className="mobileDev-Info-tabla-p">
+          <p>
+            {" "}
+            OS <div className="mobileDev-Info-tabla-OS">{mobileDev.OS}</div>
+          </p>
+          <p>
+            {" "}
+            VersionOS
+            <div className="mobileDev-Info-tabla-versionOS">
+              {mobileDev.versionOS}
+            </div>
+          </p>
+          <p>
+            {" "}
+            Language
+            <div className="mobileDev-Info-tabla-language">
+              {mobileDev.language}
+            </div>
+          </p>
+        </div>
+      </div>
       <h5>{mobileDev.description}</h5>
       <div className="button-Holder">
         <ToggleButton className="ToggleMobileDev" mobileDevId={mobileDev._id} />
